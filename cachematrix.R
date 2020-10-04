5e0dc588081c1caba68f4f8d95277d4c7c353714
samanthapeb <a00823349@itesm.mx>
2020-10-04 01:22:55 -0500

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object that can cache 
+## its inverse.
 
-## Write a short comment describing this function
+## if X is a square invertible matrix, then solve(X) returns its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matrix_inv <- NULL
+  set <- function(y) {
+    x <<- y               ##<<- different environments
+    matrix_inv <<- NULL
+  }
+  
+  get <- function() x
+  set_inv <- function(solve) matrix_inv <<- solve  ## solve calculates the inverse
+  get_inv <- function() matrix_inv                 ## get the inverse
+  list(set = set, get = get,
+       set_inv = set_inv,
+       get_inv = get_inv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed),then cacheSolve should retrieve the 
+## inverse from the cache.
+
+## most of this code is the same as the example in coursera
+
+## we want to return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  matrix_inv <- x$get_inv()         ## we want the inverse
+  if(!is.null(matrix_inv)) {        ## if it's not NULL, then it was already calculated
+    message("getting cached data")  ## if it's not NULL, it will show the cached data
+    return(matrix_inv)
+  }
+  data <- x$get()                   ## this was based on coursera's example 
+  matrix_inv <- solve(data, ...)
+  x$set_inv(matrix_inv)
+  matrix_inv                        ## print the inverse matrix
 }
+
+## To test, we need non singular and square matrix
+## How to make a matrix: "matrix(1:9, nrow = 3, ncol = 3)"
+## here is one example of a valid matrix called example_2
+## example_2 <-makeCacheMatrix(cbind(c(6,5),c(2,3)))
+
+
